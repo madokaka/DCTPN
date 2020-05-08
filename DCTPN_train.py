eccdb2abed51525a15e89790b8d7080b43d0962c
madokaka <wenboge_crrc@outlook.com>
2020-05-08 19:51:30 +0800

    update

---
+++ DCTPN_train.py
@@ -0,0 +1,365 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import argparse
+import json
+import os
+import time
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from TCN_test import TCN
+import torch.nn as nn
+from torch.utils.data import Dataset
+import h5py
+import progressbar
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--dataset',type=str,default='ActivityNet')
+# parser.add_argument('--data',type=str,default='/Activitynet/activity_net.v1-3.min.json')
+# parser.add_argument('--features',type=str,default='/Activitynet//c3d_features_Activitynet_merge.hdf5')
+# parser.add_argument('--labels',type=str,default='/Activitynet/acitivity_labels.hdf5')
+# parser.add_argument('--vid_ids',type=str,default='/Activitynet/activity_video_ids.json')
+# parser.add_argument('--k',type=int,default=64)
+parser.add_argument('--data',type=str,default='activity_net.v1-3.min.json')
+parser.add_argument('--features',type=str,default='c3d_features_Activitynet_training.hdf5')
+parser.add_argument('--labels',type=str,default='acitivity_labels_64_2.hdf5')
+parser.add_argument('--vid_ids',type=str,default='activity_video_ids_64_2.json')
+parser.add_argument('--k',type=int,default=64)
+args = parser.parse_args()
+
+
+
+train_loaded = True
+
+torch.manual_seed(1111)
+device_ids = [0, 1, 2, 3]
+num_chans = [2048,1024,512,256,128,64]
+
+
+class ProposalDataset(object):
+
+    def __init__(self,args):
+
+        self.inter = 0.5
+        self.data = json.load(open(args.data))
+        self.features = h5py.File(args.features)
+        if not os.path.exists(args.labels) or not os.path.exists(args.vid_ids):
+            self.generate_labels(args)
+            print('generate label')
+        print('label exist')
+        self.labels = h5py.File(args.labels)
+        self.vid_ids = json.load(open(args.vid_ids))
+        self.durations = {}
+        self.gt_times ={}
+        self.w1 = self.vid_ids['w1']
+        for split in ['training','validation','testing']:
+            setattr(self,split+'_ids',self.vid_ids[split])
+            for video_id in self.vid_ids[split]:
+                self.durations[video_id] = self.data['database'][video_id]['duration']
+                self.gt_times[video_id] = [ann['segment'] for ann in self.data['database'][video_id]['annotations']]
+                
+        
+            
+        
+    def generate_labels(self,args):
+        
+        label_dataset = h5py.File(args.labels,'w')
+        bar = progressbar.ProgressBar(maxval=len(self.data['database'].keys())).start()
+        prop_captured = []
+        prop_pos_examples = []
+        video_ids = self.data['database'].keys()
+        split_ids = {'training': [], 'validation': [], 'testing': [],
+                     'w1': []}
+        for progress,video_id in enumerate(video_ids):
+            try:
+                features = self.features['v_' + video_id]['c3d_features']
+                nfeats = features.shape[0]
+                duration = self.data['database'][video_id]['duration']
+                annotations = self.data['database'][video_id]['annotations']
+                timestamps = [ann['segment'] for ann in annotations]
+                featstamps = [self.timestamp_to_featstamp(x,nfeats,duration) for x in timestamps]
+                nb_prop = len(featstamps)
+                # 从尾部删除
+                for i in range(nb_prop):
+                    if (featstamps[nb_prop-i-1][1] - featstamps[nb_prop-i-1][0]>args.k/0.5):
+                        del featstamps[nb_prop-i-1]
+                if len(featstamps) == 0:
+                    if len(timestamps) == 0:
+                        prop_captured +=[-1.0]
+                    else:
+                        prop_captured +=[0.0]
+                    continue
+                split_ids[self.data['database'][video_id]['subset']] += [video_id]
+                labels = np.zeros((nfeats,args.k))
+                gt_captured = []
+                for t in range(nfeats):
+                    for k in range(args.k):
+                        iou,gt_index = self.iou([t-(k/self.inter),t+1],featstamps,return_index=True)
+                        if iou >= 0.5:
+                            labels[t,k] = 1
+
+                            gt_captured += [gt_index]
+                prop_captured += [1.* len(np.unique(gt_captured))/len(timestamps)]
+                if self.data['database'][video_id]['subset'] == 'training':
+                    prop_pos_examples += [np.sum(labels,axis=0)*1./nfeats]
+                video_dataset = label_dataset.create_dataset(video_id,(nfeats,args.k),dtype='f')
+                video_dataset[...] = labels
+                bar.update(progress)
+            except:
+                print('pass vidoe_id:',video_id)
+                pass
+        split_ids['w1'] = np.array(prop_pos_examples).mean(axis=0).tolist()
+        json.dump(split_ids,open(args.vid_ids,'w'))
+        bar.finish()
+                    
+            
+            
+    def timestamp_to_featstamp(self,timestamp,nfeats,duration):
+        start,end = timestamp 
+        start = min(int(round(start/duration*nfeats)),nfeats - 1)
+        end = max(int(round(end/duration*nfeats)),start + 1)
+        return start,end
+    
+    def iou(self,interval,featstamps,return_index=False):
+
+        start_i,end_i = interval[0],interval[1]        
+        output = 0.0
+        gt_index = -1
+        for i,(start,end) in enumerate(featstamps):
+            intersection = max(0,min(end,end_i) - max(start,start_i))
+            union = min(max(end,end_i) - min(start,start_i),end- start + end_i - start_i)
+            overlap = float(intersection)/(union+1e-8)
+            if overlap >= output:
+                output = overlap
+                gt_index = i
+        if return_index:
+            return output,gt_index
+        return output
+
+
+
+class Datasplit(Dataset):
+    
+    def collate_fn(self,data):
+        
+        features = [d[0] for d in data]
+        masks = [d[1] for d in data]
+        labels = [d[2] for d in data]
+        
+        return torch.cat(features,0),torch.cat(masks,0),torch.cat(labels,0)
+    
+    
+    def __init__(self,video_ids,dataset,args):
+        self.video_ids = video_ids
+        self.features = dataset.features
+        self.labels = dataset.labels
+        self.durations = dataset.durations
+        
+    def __getitem__(self,index):
+        video_id = self. video_ids[index]
+        features = self.features['v_'+video_id]['c3d_features']
+        labels = self.labels[video_id]
+        nfeats = features.shape[0]
+        nwindows = max(1,nfeats - 128 + 1)
+        sample = range(nwindows)
+        if nwindows > 256:
+            sample = np.random.choice(nwindows,256)
+            nwindows = 256
+            
+        mask = np.zeros((256,128,args.k))
+        for i in range(128):
+            for a in range((i + 3) // 2):
+                mask[:, i, :min(64, a)] = 1
+
+        mask = torch.FloatTensor(mask)
+        
+        
+        mask = mask[:nwindows,:,:]
+        
+        feature_windows = np.zeros((nwindows,128,features.shape[1]))
+        label_windows = np.zeros((nwindows,128,args.k))
+
+
+        for j,w_start in enumerate(sample):
+            w_end = min(w_start+128,nfeats)
+            feature_windows[j,0:w_end-w_start,:] = features[w_start:w_end,:]
+            label_windows[j,0:w_end-w_start,:] = labels[w_start:w_end,:]
+
+
+            
+        return torch.FloatTensor(feature_windows),mask,torch.Tensor(label_windows)
+
+    def __len__(self):
+        
+        return len(self.video_ids)
+
+class Evaluatesplit(Dataset):
+
+    def __init__(self,video_ids,dataset,args):
+        self.video_ids = video_ids
+        self.features = dataset.features
+        self.labels = dataset.labels
+        self.durations = dataset.durations
+        self.gt_times = dataset.gt_times
+    
+    def collate_fn(self,data):
+        features = data[0][0]
+        gt_times = data[0][1]
+        durations = data[0][2]
+        
+        return features.view(1,features.size(0),features.size(1)),gt_times,durations
+    
+
+    def __getitem__(self,index):
+        video_id = self.video_ids[index]
+        features = self.features['v_'+video_id]['c3d_features']
+        duration = self.durations[video_id]
+        gt_times = self.gt_times[video_id]
+        
+        return torch.FloatTensor(features),gt_times,duration
+    
+
+    def __len__(self):
+        return len(self.video_ids)
+
+
+def train(epoch,w1):
+    model.train()
+    total_loss = []
+    model.train()
+    start_time = time.time()
+    for batch_idx,(features,masks,labels) in enumerate(train_loader):
+        features = features.cuda()
+        labels = labels.cuda()
+        masks = masks.cuda()
+        features = Variable(features)
+        optimizer.zero_grad()
+        # print(features.size())
+        proposals = model(features)
+
+        loss = compute_loss_with_BCE(proposals,masks,labels,w1)
+
+    
+        loss.backward()
+        optimizer.step()
+        total_loss.append(loss.item())
+
+        if batch_idx % 100 == 0:
+            cur_loss = total_loss[-1]
+            log_entry = ('epoch{:3d} | {:5d}/{:5d} | lr {:2.4f} | \
+                  loss {:5.6f} | time: {:5.2f}').format(epoch,batch_idx,len(train_loader),0.1,cur_loss*1000,
+                  (time.time()-start_time))
+            print(log_entry)
+            with open('train_128.log','a') as f:
+                f.write(log_entry)
+                f.write('\n')
+            start_time = time.time()
+
+def evaluate(data_loader,maximum=None):
+    model.eval()
+    total = len(data_loader)
+    if maximum is not None:
+        total = min(total,maximum)
+    recall = np.zeros(total)
+    for batch_idx,(f,g,d) in enumerate(data_loader):
+        if maximum is not None and batch_idx >= maximum:
+            break
+        f = f.cuda()
+        features = Variable(f)
+        proposals = model(features)
+        recall[batch_idx] = calculate_stats(proposals,g,d,args)
+    return np.mean(recall)
+
+def calculate_stats(proposals,gt_times,duration,args):
+    timestamps = proposals_to_timestamps(proposals.data,duration)
+    gt_detected = np.zeros(len(gt_times))
+    for i,timestamp in enumerate(timestamps):
+        iou_i,k = dataset.iou(timestamp,gt_times,return_index=True)
+        if iou_i >0.5:
+            gt_detected[k] = 1
+    return gt_detected.sum()*1./len(gt_detected)
+
+def proposals_to_timestamps(proposals,duration,num_proposals=None):
+    _,nb_steps,k = proposals.size()
+    if num_proposals and num_proposals < nb_steps*k:
+        sort,_ = proposals.view(nb_steps*k).sort()
+        score_threshold = sort[-num_proposals]
+        proposals = proposals >= score_threshold
+    step_length = duration/nb_steps
+    timestamps = []
+    for time_step in np.arange(nb_steps):
+        p = proposals[0,time_step]
+        if p.sum() != 0:
+            end = time_step*step_length
+            for k in np.arange(k):
+                if p[k] != 0:
+                    start = max(0,time_step-k-1)*step_length
+                    timestamps.append((start,end))
+    return timestamps
+
+def compute_loss_with_BCE(outputs,masks,labels,w1):
+        
+    w1 = torch.FloatTensor(w1).type_as(outputs.data)
+    
+    w0 = 1.0 - w1
+    labels = labels.mul(masks)
+    
+    weights = labels.mul(w0.expand(labels.size()))+(1.-labels).mul(w1.expand(labels.size()))
+    weights = weights
+    masks = Variable(masks)
+    labels = Variable(labels)
+    outputs = outputs.mul(masks)
+    criterion = torch.nn.BCELoss(weight=weights)
+    loss = criterion(outputs,labels)
+    
+    return loss
+
+
+        
+dataset = ProposalDataset(args)
+w1 = dataset.w1
+train_dataset = Datasplit(dataset.training_ids, dataset, args)
+val_dataset = Evaluatesplit(dataset.validation_ids,dataset,args)
+train_loader = DataLoader(train_dataset,batch_size=1,collate_fn=train_dataset.collate_fn)
+
+
+if train_loaded == True:
+    model = TCN(4096, num_chans)
+    model = torch.nn.DataParallel(model, device_ids=device_ids)
+    model.cuda()
+    model.load_state_dict(torch.load('save_load_test.pth'))
+else:
+    model = TCN(4096,num_chans)
+    model.cuda()
+    model = torch.nn.DataParallel(model, device_ids=device_ids)
+
+
+
+
+epoch_num = 0
+
+optimizer = optim.SGD(model.parameters(),lr=0.01,momentum=0.9,weight_decay=0)
+for epoch in range(epoch_num,10000):
+    epoch_start_time = time.time()
+    train(epoch,w1)
+    if epoch % 10 == 0  and epoch >0:
+       
+
+        log_entry = ('|end of epoch {:3d} | time:{:5.2f}s | '.format(epoch,(time.time()-epoch_start_time)))
+        print('='*89)
+        print(log_entry)
+        print('='*89)
+        with open('val_6layer_train_1.log', 'a') as f:
+            f.write(log_entry)
+            f.write('\n')
+
+        torch.save(model.state_dict(), os.path.join('6layer_64_2_model_' + str(epoch) + '.pth'))
+
+        print('save_model')
---
+++ feature_extractor_another_gpu_0.py
@@ -0,0 +1,175 @@
+# coding: utf-8
+from C3D_model import *
+import torchvision
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import argparse
+import os
+from torch import save, load
+import pickle
+import time
+import numpy as np
+import PIL.Image as Image
+import skimage.io as io
+from skimage.transform import resize
+import h5py
+from PIL import Image
+import re
+import progressbar
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+def feature_extractor():
+	net = C3D(487)
+	print('net', net)
+	## Loading pretrained model from sports and finetune the last layer
+	net.load_state_dict(torch.load('c3d.pickle'))
+	RUN_GPU = 1
+	if RUN_GPU :
+		net.cuda()
+		net.eval()
+		print('net-gpu', net)
+	feature_dim = 4096 if EXTRACTED_LAYER != 5 else 8192
+
+	# read video list from the folder
+	# read video list from the txt list
+	video_list_file = args.video_list_file
+	video_list = open(video_list_file).readlines()
+	video_list = [item.strip() for item in video_list]
+	video_list = video_list[4000:]
+	print('video_list', video_list)
+
+	gpu_id = args.gpu_id
+
+	if not os.path.isdir(OUTPUT_DIR):
+		os.mkdir(OUTPUT_DIR)
+	f = h5py.File(os.path.join(OUTPUT_DIR, OUTPUT_NAME), 'a')
+
+	# current location
+	temp_path = 'D:\\temp'
+	if not os.path.exists(temp_path):
+		os.mkdir(temp_path)
+
+	error_fid = open('error.txt', 'a')
+	for video_name in video_list:
+
+		video_path = os.path.join(VIDEO_DIR, video_name)
+		print('video_path', video_path)
+		frame_path = os.path.join(temp_path, video_name)
+		print('frame-path'+frame_path)
+		if not os.path.exists(frame_path):
+			os.mkdir(frame_path)
+
+
+		print('Extracting video frames ...')
+		# using ffmpeg to extract video frames into a temporary folder
+		# example: ffmpeg -i video_validation_0000051.mp4 -q:v 2 -f image2 output/image%5d.jpg
+		os.system('ffmpeg -i ' + video_path + ' -r 16 -q:v 2 -f image2 ' + frame_path + '/image_%5d.jpg')
+
+
+		print('Extracting features ...')
+		total_frames = len(os.listdir(frame_path))
+		if total_frames == 0:
+			error_fid.write(video_name+'\n')
+			print('Fail to extract frames for video: %s'%video_name)
+			continue
+		if total_frames <16:
+			print('frame less than 16')
+			with open('less_than_16_error.txt', 'a') as f:
+				f.write(video_name+'\n')
+			continue
+
+		valid_frames = total_frames / nb_frames * nb_frames
+		n_feat = valid_frames / nb_frames
+		n_batch = n_feat / BATCH_SIZE
+		if n_feat - n_batch*BATCH_SIZE > 0:
+			n_batch = n_batch + 1
+		print('n_frames: %d; n_feat: %d; n_batch: %d'%(total_frames, n_feat, n_batch))
+
+		index_w = np.random.randint(resize_w - crop_w) ## crop
+		index_h = np.random.randint(resize_h - crop_h) ## crop
+
+		features = []
+
+		n_batch = int(n_batch)
+		bar = progressbar.ProgressBar(maxval=n_batch).start()
+		for progress,i in enumerate(range(n_batch-1)):
+			input_blobs = []
+			bar.update(progress)
+			for j in range(BATCH_SIZE):
+				clip = []
+				clip = np.array([resize(io.imread(os.path.join(frame_path, 'image_{:05d}.jpg'.format(k))), output_shape=(resize_w, resize_h), preserve_range=True) for k in range((i*BATCH_SIZE+j) * nb_frames+1, min((i*BATCH_SIZE+j+1) * nb_frames+1, valid_frames+1))])
+
+				clip = clip[:, index_w: index_w+ crop_w, index_h: index_h+ crop_h, :]
+				input_blobs.append(clip)
+			input_blobs = np.array(input_blobs, dtype='float32')
+			input_blobs = torch.from_numpy(np.float32(input_blobs.transpose(0, 4, 1, 2, 3)))
+			input_blobs = Variable(input_blobs).cuda() if RUN_GPU else Variable(input_blobs)
+			_, batch_output = net(input_blobs, EXTRACTED_LAYER)
+			batch_feature  = (batch_output.data).cpu()
+			features.append(batch_feature)
+
+
+
+		input_blobs = []
+		n_feat = int(n_feat)
+		n_batch = int(n_batch)
+		for j in range(n_feat-(n_batch-1)*BATCH_SIZE):
+			clip = []
+			clip = np.array([resize(io.imread(os.path.join(frame_path, 'image_{:05d}.jpg'.format(k))), output_shape=(resize_w, resize_h), preserve_range=True) for k in range(((n_batch-1)*BATCH_SIZE+j) * nb_frames+1, min(((n_batch-1)*BATCH_SIZE+j+1) * nb_frames+1, valid_frames+1))])
+
+			clip = clip[:, index_w: index_w+ crop_w, index_h: index_h+ crop_h, :]
+
+			input_blobs.append(clip)
+		input_blobs = np.array(input_blobs, dtype='float32')
+
+		input_blobs = torch.from_numpy(np.float32(input_blobs.transpose(0, 4, 1, 2, 3)))
+		input_blobs = Variable(input_blobs).cuda() if RUN_GPU else Variable(input_blobs)
+		_, batch_output = net(input_blobs, EXTRACTED_LAYER)
+		batch_feature  = (batch_output.data).cpu()
+		print(batch_feature.shape)
+		features.append(batch_feature)
+
+		features = torch.cat(features, 0)
+		features = features.numpy()
+		print('features', features)
+		video_name = re.sub(r'.avi|.mp4','',video_name)
+		print(video_name)
+		fgroup = f.create_group('v_'+ video_name)
+		fgroup.create_dataset('c3d_features', data=features)
+		fgroup.create_dataset('total_frames', data=np.array(total_frames))
+		fgroup.create_dataset('valid_frames', data=np.array(valid_frames))
+
+		bar.finish()
+		print ('%s has been processed...'%video_name)
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+
+	print ('******--------- Extract C3D features ------*******')
+	parser.add_argument('-o', '--OUTPUT_DIR', dest='OUTPUT_DIR', type=str, default='output_frm/', help='Output file name')
+	parser.add_argument('-l', '--EXTRACTED_LAYER', dest='EXTRACTED_LAYER', type=int, choices=[5, 6, 7, 8], default=7, help='Feature extractor layer')
+	parser.add_argument('-i', '--VIDEO_DIR', dest='VIDEO_DIR', type = str, default='E:/activitynet_dataset/new_testing/',help='Input Video directory')
+	parser.add_argument('-gpu', '--gpu', dest='GPU', action = 'store_true', help='Run GPU?')
+	parser.add_argument('--OUTPUT_NAME', default='c3d_features_Activitynet_testing_sm_4000_end.hdf5', help='The output name of the hdf5 features')
+	parser.add_argument('-b', '--BATCH_SIZE', default=1, help='the batch size')
+	parser.add_argument('-id', '--gpu_id', default=0, type=int)
+	parser.add_argument('-p', '--video_list_file', type=str,default= 'video_list_testing.txt' ,help='the video name list')
+
+	args = parser.parse_args()
+	params = vars(args) # convert to ordinary dict
+	print ('parsed parameters:')
+
+	OUTPUT_DIR = params['OUTPUT_DIR']
+	EXTRACTED_LAYER = params['EXTRACTED_LAYER']
+	VIDEO_DIR = params['VIDEO_DIR']
+	RUN_GPU = params['GPU']
+	OUTPUT_NAME = params['OUTPUT_NAME']
+	BATCH_SIZE = params['BATCH_SIZE']
+	crop_w = 112
+	resize_w = 128
+	crop_h = 112
+	resize_h = 171
+	nb_frames = 16
+	feature_extractor()
